21fa03f58cd054de57de6e7a4fb3a9a0e3780c7a
Anders <joensson.anders@gmail.com>
2016-04-24 21:14:04 +0200

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
 var sassStyle = 'compressed';
-
-gulp.task('default', ['sass'], function(){
-});
-
-var sass = require('gulp-ruby-sass');
-gulp.task('sass', function (style, asd, assd) {
-    gutil.log('style:' + sassStyle);
-    gulp.src('./styles/app.scss')
-        .pipe(sass({style: sassStyle}))
-        .on('error', gutil.log)
-        .pipe(gulp.dest('./styles/output'));
-});
-
 var csslint = require('gulp-csslint');
-gulp.task('csslint', function() {
-    gulp.src('./styles/app-imports.scss')
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', () => {
+    gutil.log('style:' + sassStyle);
+    return gulp.src('./styles/app.scss')
+        .pipe(sass({outputStyle: sassStyle})
+            .on('error', sass.logError))
+        .pipe(gulp.dest('./output'));
+});
+
+gulp.task('csslint', () => {
+    return gulp.src('./styles/app-imports.scss')
         .pipe(sass())
         .pipe(gulp.dest('./styles/output'))
         .pipe(csslint('.csslintrc'))
         .pipe(csslint.reporter());
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     sassStyle = 'expanded';
     gulp.watch('./styles/**/*.scss', ['sass']);
 });
